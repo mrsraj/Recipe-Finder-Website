a64a46ea43a236f42bb5112a8f17076c684ea950
Sachin Raj <sachinraj6392@gmail.com>
2024-03-12 03:29:27 +0530

    make card and add css for them

--- RecipeJS.js
+++ RecipeJS.js
@@ -1,82 +1,46 @@
-async function searchMeal() {
-
-    let mealspage = document.getElementById("meal-container");
-    // mealspage.innerHTML = '';
-
+async function defaultFun() {
+    let selectDiv = document.getElementById('result');
+    selectDiv.textContent = "";
     const searchInput = document.getElementById("search").value;
-
-     
-
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
+    let data = await response.json();
 
-    let Data = await response.json();
-    console.log(Data);
-    console.log(Data.meals);
-    mealspage.textContent="Data.meals";
+    data.meals.forEach((element) => {
+        let newDiv = document.createElement('div');
+        newDiv.className = 'cardAll';
 
-   
+        let htmlContent = `
+                <h2>${element.strMeal}</h2>
+                <img src="${element.strMealThumb}" alt="${element.strMeal}">
+                <p>${element.strInstructions}</p>
+            `;
 
-    // finalData.meals.forEach((Data) => {
-    //     console.log(Data.strMeal);
-
-    //     let data1 = document.createElement('p');
-    //     data1.innerHTML = Data.strMeal;
-    //     mealspage.appendChild('data1')
-
-    // });
-    
-
-    // if (finalData.meals) {
-    //     data.forEach(meal => {
-    //         const mealDiv = document.createElement('div');
-    //         mealDiv.classList.add('meal');
-    //         mealDiv.innerHTML = `
-    //     <h3>${meal.strMeaL}</h3>
-    //     <img src = "${meal.strMeaLThumb}">
-    //     <p>${meal.strInstructions}</p>
-
-    //     `;
-    //         div.appendChild(mealDiv);
-    //     })
-    // }
-    // else {
-    //     div.innerHTML = '<p>No meals found. please try again.</p>';
-    // }
-
+        newDiv.innerHTML = htmlContent;
+        selectDiv.appendChild(newDiv);
+    });
 }
 
+defaultFun();
 
+async function searchMeal() {
+    let selectDiv = document.getElementById('result');
+    selectDiv.textContent = "";
+    const searchInput = document.getElementById("search").value;
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`);
+    let data = await response.json();
 
+    data.meals.forEach((element) => {
+        let newDiv = document.createElement('div');
+        newDiv.className = 'cardAll';
 
+        let htmlContent = `
+                <h2>${element.strMeal}</h2>
+                <img src="${element.strMealThumb}" alt="${element.strMeal}">
+                <p>${element.strInstructions}</p>
+            `;
 
+        newDiv.innerHTML = htmlContent;
+        selectDiv.appendChild(newDiv);
+    });
+}
 
-
-
-
-
-
-
-
-
-// .then(response => response.json())
-// .then(data =>{
-//     console.log(data);
-
-//    if(data.meals)
-//    {
-//     data.forEach(meal => {
-//         const mealDiv = document.createElement('div');
-//         mealDiv.classList.add('meal');
-//         mealDiv.innerHTML = `
-//         <h3>${meal.strMeaL}</h3>
-//         <img src = "${meal.strMeaLThumb}">
-//         <p>${meal.strInstructions}</p>
-
-//         `;
-//         div.appendChild(mealDiv);
-//
-//    }
-//    else{
-//        div.innerHTML='<p>No meals found. please try again.</p>';
-//    }
-// })
